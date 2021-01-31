405ee273cb9f88626dbafb40040a73baf9c8dfa7
zabalueva <age_eva@mail.ru>
2021-01-31 18:45:42 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array == undefined || !array.length) {
+        return 0;
+    } else
+        return array.reduce((min, current) => current < min ? min = current : min);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == undefined || !array.length) {
+        return 0;
+    } else
+        return array.reduce((max, current) => current > max ? max = current : max);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+exports.avg = function avg(array) {
+    if (array == undefined || !array.length) {
+        return 0;
+    } else
+        return array.reduce((sum, current) => (sum + current)) / array.length.toFixed(1);
 }
